af8e01356fafacbd41c020840ffeb89cf147bd2d
Adam Plansky <adamplansky@gmail.com>
2017-11-24 08:30:54 +0100

    add script repeat

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const { Client } = require('tplink-smarthome-api');
-
 const client = new Client();
-const plug = client.getDevice({host: '192.168.13.79'}).then((device)=>{
-  device.getSysInfo().then(console.log);
-  //on / off
-//   device.setPowerState(true);
-  device.emeter.getRealtime().then(console.log);
-});
 
+
+const host_ip = '192.168.13.79'
+
+
+const plug = client.getDevice({host: host_ip}).then((device)=>{
+  device.emeter.getRealtime().then(function(val){
+    var json_data = JSON.parse(JSON.stringify(val));    
+    json_data['date'] = new Date();
+    console.log(JSON.stringify(json_data))
+  });
+});
---
+++ repeat.sh
@@ -0,0 +1,6 @@
+while true
+  do 
+    node index.js >> log.txt
+    sleep 1
+  done
+done
